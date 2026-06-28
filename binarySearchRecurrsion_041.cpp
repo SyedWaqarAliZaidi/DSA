1aa9b667724a612a2c04bf2201e9cc033e1d6575
SyedWaqarAliZaidi <zaidialiwaqarsyed2006@gmail.com>
2026-06-28 09:19:14 +0500

    Refactor binary search to use recursion

--- binarySearchRecurrsion_041.cpp
+++ binarySearchRecurrsion_041.cpp
@@ -1,29 +1,33 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 int binarySearch(vector<int> arr, int tar,int start, int end)
 {
  
   while(start <= end)
-  mid = start + (end - start)/2;
-  if(arr[mid] < tar)
   {
-    binarySearch( arr, int tar, int start, int end)
+    int mid = start + (end - start)/2;
+    if(arr[mid] < tar)
+      {
+        return binarySearch(arr, tar, mid + 1, end);
+      }
+    else if(arr[mid] > tar)
+      {
+        return binarySearch(arr, tar, start, mid - 1);
+      }
+    else
+      {
+        return mid;
+      }
   }
-  else if(arr[mid] > tar)
-  {
-    binarySearch( arr, int tar, int start, int end)
-  }
-  else;
-  {
-    return mid;
-  }
-return -1;
+  return -1;
 }
+
 int main()
 {
     vector<int> arr = {1,3,5,7,9,11};
-    int tar = 7;
-     int start = 0, end = arr.size() - 1;
+    int tar = 11;
+    int start = 0, end = arr.size() - 1;
     cout << binarySearch(arr, tar, start, end) << endl;
     return 0;
 }
