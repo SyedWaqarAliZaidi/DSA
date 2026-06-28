f80449c6d5b11e6b305030e975fa8347218fef92
Syed Waqar Ali Zaidi <zaidialiwaqarsyed2006@gmail.com>
2026-06-28 17:34:48 +0500

    Update doublyLinkedList_041.cpp

--- doublyLinkedList_041.cpp
+++ doublyLinkedList_041.cpp
@@ -1,87 +1,104 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 class Node {
-    public:
+public:
     int data;
     Node* next;
     Node* prev;
 
-    Node(int val){
+    Node(int val) {
         data = val;
-        next = null;
-        prev = null;
-    
+        next = nullptr;
+        prev = nullptr;
     }
 };
 
-class DoublyList
-{
-    head = tail = null;
+class DoublyList {
+public:
+    Node* head;
+    Node* tail;
 
-    push_front(int val){
+    DoublyList() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void push_front(int val) {
         Node* newNode = new Node(val);
-        if(head == null){
+        if (head == nullptr) {
             head = tail = newNode;
         }
-        else{
+        else {
             newNode->next = head;
             head->prev = newNode;
-            head = newnode;
+            head = newNode;
         }
-        }
-   push_back(int val){
+    }
+
+    void push_back(int val) {
         Node* newNode = new Node(val);
-        if(head == null){
+        if (head == nullptr) {
             head = tail = newNode;
         }
-        else{
-            newNode->prev=tail;
+        else {
+            newNode->prev = tail;
             tail->next = newNode;
             tail = newNode;
         }
-        }
-        pop_front(){
-            if(head = null){
-                cout << "list is empty";
-                return;
-            }
-            else if(head = tail){
-                delete head;
-                head = tail = null;
-            }
-            else{
-                Node* temp = head;
-                head = head->next;
-                head->prev = null;
-                delete temp;
-            }
-        }
+    }
 
-
-        pop_back(){
-        
-            if(head = null){
-                cout << "list is empty";
-                return;
-            }
-            else if(head = tail){
-                delete head;
-                head = tail = null;
-            }
-            else{
-                Node* temp = tail;
-                tail = tail->prev;
-                tail->next = null;
-                delete temp;
-            }
+    void pop_front() {
+        if (head == nullptr) {
+            cout << "list is empty" << endl;
+            return;
+        }
+        else if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+        }
+        else {
+            Node* temp = head;
+            head = head->next;
+            head->prev = nullptr;
+            delete temp;
         }
     }
-int main()
-{
+
+    void pop_back() {
+        if (head == nullptr) {
+            cout << "list is empty" << endl;
+            return;
+        }
+        else if (head == tail) {
+            delete head;
+            head = tail = nullptr;
+        }
+        else {
+            Node* temp = tail;
+            tail = tail->prev;
+            tail->next = nullptr;
+            delete temp;
+        }
+    }
+    void printList() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data << "->";
+            temp=temp->next;
+
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
     DoublyList dll;
-    dll.pushfront(2);
-    dll.pushfront(1);
-    dll.pushback(3);
-    dll.pushback(4);
+    dll.push_front(2);
+    dll.push_front(1);
+    dll.push_back(3);
+    dll.push_back(4);
+    dll.printList();
+    cout << endl;
     return 0;
 }
