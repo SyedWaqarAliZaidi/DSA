a42456d0e72b3edf20befed4ca3081626dc3436c
SyedWaqarAliZaidi <zaidialiwaqarsyed2006@gmail.com>
2026-06-28 09:09:25 +0500

    Fix binary search implementation and formatting

--- binarySearchIterative_041.cpp
+++ binarySearchIterative_041.cpp
@@ -4,22 +4,25 @@
 using namespace std;
 int binarySearch(const vector<int>& arr, int tar)
 {
-  int start = 0, end = arr.size() - 1;
-  while(start <= end)
-  mid = start + (end - start)/2;
-  if(arr[mid] < tar)
-  {
-    end = mid - 1;
-  }
-  else if(arr[mid] > tar)
-  {
-    start = mid + 1;
-  }
-  else
-  {
-    return mid;
-  }
-return -1;
+    int start = 0, end = arr.size() - 1;
+    while(start <= end)
+       {
+        int mid = start + (end - start)/2;
+        if(arr[mid] > tar)
+          {
+           end = mid - 1;
+          }
+        else if(arr[mid] < tar)
+          {
+           start = mid + 1;
+          }
+        else
+          {
+           return mid;
+          }
+       }   
+    return -1;
+  
 }
 int main()
 {
